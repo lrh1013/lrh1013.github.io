4ed3c5f4eb2e9b6271fe14bf9261afd2d92a36ad
lrh1013 <dolefulmoon@gmail.com>
2026-01-09 00:52:06 +0900

    fix: 마운트 방식 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,19 @@
+import { useLocation } from 'react-router-dom';
 import Hero from '@/components/home/Hero.tsx';
 import Project from '@/components/home/Project.tsx';
 import Experience from '@/components/home/Experience.tsx';
 import LiquidEther from '@/components/common/LiquidEther.tsx';
 
 function Home() {
+  const location = useLocation();
+
   return (
     <>
       <div className="fixed left-0 top-0 w-full h-screen z-1">
-        <LiquidEther colors={['#a78bfa', '#d5ccf5']} />
+        <LiquidEther
+          key={location.key}
+          colors={['#a78bfa', '#d5ccf5']}
+        />
       </div>
       <div className="relative z-2">
         <Hero />
